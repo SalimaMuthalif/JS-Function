b6dc27596b3a64dae448265b792c95903b3ca203
SalimaMuthalif <salimasalimam761@gmail.com>
2024-09-28 11:00:11 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,28 @@
+// 1:concatenation;    
+function concatsrings(string1,string2) {
+    return string1+string2;
+    }
+    console.log(concatsrings("Hello"," World"));
+    console.log(concatsrings("welcome "," kerala"));
+    
+// 2:function divide;
+    function divide(num1,num2) {
+    return num1/num2;        
+    }        
+    console.log(divide(50,2));
+    console.log(divide(75,5));
+    
+// 3:calculate Area;
+    function calculateArea(length,width) {
+    return length*width;    
+    }    
+    console.log(calculateArea(8,6));
+    console.log(calculateArea(4,6));
+
+// 4:Arrow function;    
+    const double=(num)=>num*2;
+    console.log(double(5));
+    console.log(double(9));
+
+    
+    
